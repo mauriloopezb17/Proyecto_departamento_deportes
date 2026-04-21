cd54196752684f0ecf42701c883dfa28e2a3df5a
Pancake99 <alejandro.a.bobarin.marquez@gmail.com>
2026-04-21 09:40:06 -0400

    enlace front y Oauth2

--- packages/frontend/src/LoginPage.tsx
+++ packages/frontend/src/LoginPage.tsx
@@ -142,7 +142,7 @@ function LoginPage() {
             <button
               type="button"
               className="google-btn"
-              onClick={() => console.log('Login con Google')}
+              onClick={() => window.location.href = 'http://146.235.242.100:3001/api/auth/google'}
             >
               <svg viewBox="0 0 24 24" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
